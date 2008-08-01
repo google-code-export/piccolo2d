8e173b3de5055fdcbeba59a63f0638cfe65af259
mrohrmoser@gmx.de <mrohrmoser@gmx.de@aadc08cf-1350-0410-9b51-cf97fce99a1b>
2008-08-01 00:47:57 +0000

    issue#42 learning center learn/index.html

--- site/svn-mime-types.sh
+++ site/svn-mime-types.sh
@@ -9,7 +9,7 @@ find $dir -name "*.gif" | xargs $svn propset svn:mime-type image/gif
 find $dir -name "*.html" | xargs $svn propset svn:mime-type text/html
 find $dir -name "*.jar" | xargs $svn propset svn:mime-type application/x-java-archive
 find $dir -name "*.jpeg" -or -name "*.jpg" | xargs $svn propset svn:mime-type image/jpeg
-find $dir -name "*.js" | xargs $svn propset svn:mime-type application/x-javascript
+find $dir -name "*.js" | xargs $svn propset svn:mime-type text/javascript
 find $dir -name "*.pdf" | xargs $svn propset svn:mime-type application/pdf
 find $dir -name "*.png" | xargs $svn propset svn:mime-type image/png
 find $dir -name "*.txt" | xargs $svn propset svn:mime-type text/plain
