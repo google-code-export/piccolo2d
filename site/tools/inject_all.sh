96dcffbdaa98fb1615ed3ed5595c0b85c2e38e89
allain.lalonde <allain.lalonde@aadc08cf-1350-0410-9b51-cf97fce99a1b>
2009-10-21 16:20:42 +0000

    Adding java applet for fisheye and adjusting navigation accordingly, also changed link to .NET Help to new version.

--- site/tools/inject_all.sh
+++ site/tools/inject_all.sh
@@ -3,6 +3,6 @@
 #
 # must be called from the site's root.
 #
-tools/inject_navigation.rb *.html applications/*.html learn/*.html play/*.html play/applet/*.html
-for f in *.html applications/*.html learn/*.html play/*.html play/applet/*.html; do echo $f;tidy -asxhtml -m -i -wrap 100 $f; done
+tools/inject_navigation.rb *.html applications/*.html learn/*.html play/*.html play/java/*.html play/dotnet/*.html
+for f in *.html applications/*.html learn/*.html play/*.html play/java/*.html play/dotnet/*.html; do echo $f;tidy -asxhtml -m -i -wrap 100 $f; done
 
